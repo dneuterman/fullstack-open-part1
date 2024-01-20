d612c45aede02bf3d369209d7e0d0394dbda6411
dneuterman <dneuterman@gmail.com>
2024-01-20 11:05:53 -0500

    courseinfo 1.2 complete

--- courseinfo/src/App.jsx
+++ courseinfo/src/App.jsx
@@ -6,10 +6,25 @@ const Header = (props) => {
   )
 }
 
-const Content = (props) => {
+const Part = (props) => {
   return (
     <>
     <p>{props.part} {props.exercises}</p>
+    </>
+  )
+}
+
+const Content = ({part, exercises}) => {
+  let content = [];
+  for (let i = 0; i < part.length; i++) {
+    content.push({
+      part: part[i],
+      exercises: exercises[i]
+    })
+  }
+  return (
+    <>
+    {content.map((item, index) => <Part key={index} part={item.part} exercises={item.exercises} />)}
     </>
   )
 }
@@ -38,9 +53,7 @@ const App = () => {
   return (
     <div>
       <Header cours={course}/>
-      <Content part={part1} exercises={exercises1} />
-      <Content part={part2} exercises={exercises2} />
-      <Content part={part3} exercises={exercises3} />
+      <Content part={[part1, part2, part3]} exercises={[exercises1, exercises2, exercises3]} />
       <Total total={[exercises1, exercises2, exercises3]} />
     </div>
   )
