fd0420885d7683152b635280c38186502d9babe5
dneuterman <dneuterman@gmail.com>
2024-01-20 18:58:00 -0500

    started part anecdotes exercise

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,32 +1,62 @@
 import { useState } from 'react';
 
-const Display = ({ counter }) => {
+const History = (props) => {
+  if (props.allClicks.length == 0) {
+    return (
+      <div>
+        The app is used by pressing the buttons
+      </div>
+    )
+  }
+
   return (
-    <div>{counter}</div>
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
   )
 }
 
-const Button = ({ onClick, text }) => {
+const Button = ({ handleClick, text }) => {
   return (
-    <button onClick={onClick}>
+    <button onClick={handleClick}>
       {text}
     </button>
   )
 }
 
-const App = () => {
-  const [counter, setCounter] = useState(0);
+const helloClick = (name) => {
+  console.log(`Hello ${name}`);
+}
 
-  const increaseByOne = () => setCounter(counter + 1);
-  const decreaseByOne = () => setCounter(counter - 1);
-  const setToZero = () => setCounter(0);
+const App = () => {
+  const [left, setLeft] = useState(0);
+  const [right, setRight] = useState(0);
+  const [allClicks, setAll] = useState([]);
+  const [total, setTotal] = useState(0);
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'));
+    const updatedLeft = left + 1;
+    setLeft(updatedLeft);
+    setTotal(updatedLeft + right);
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'));
+    const updatedRight = right + 1;
+    setRight(updatedRight);
+    setTotal(left + updatedRight);
+  }
 
   return (
     <div>
-      <Display counter={counter} />
-      <Button onClick={increaseByOne} text="plus" />
-      <Button onClick={decreaseByOne} text="minus" />
-      <Button onClick={setToZero} text="zero" />
+      {left}
+      <Button handleClick={handleLeftClick} text="left" />
+      <Button handleClick={handleRightClick} text="right" />
+      {right}
+      <History allClicks={allClicks} />
+      <p>total {total}</p>
+      <Button handleClick={() => helloClick("Donald")} text="Hello" />
     </div>
   )
 }
